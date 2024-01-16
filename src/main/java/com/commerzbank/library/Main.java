af5e10ba90855f9ba72bd87da91ca2ef83c02fff
Kacper Bednarski <kacper.bednarski.bus@gmail.com>
2024-01-16 15:08:12 +0100

    Added usage of lambda and stream

--- src/main/java/com/commerzbank/library/Main.java
+++ src/main/java/com/commerzbank/library/Main.java
@@ -1,7 +1,17 @@
 package com.commerzbank.library;
 
+import com.commerzbank.library.book.BookRepository;
+import com.commerzbank.library.book.BookRepositoryImpl;
+import com.commerzbank.library.book.BookService;
+import com.commerzbank.library.book.SearchCriteria;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SearchCriteria searchCriteria = SearchCriteria.builder()
+                        .author("Jan Kowalski")
+                                .build();
+        BookRepository bookRepository = new BookRepositoryImpl();
+        BookService bookService = new BookService(bookRepository);
+        System.out.println(bookService.findBooks(searchCriteria));
     }
 }
